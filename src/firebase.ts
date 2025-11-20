c3cd8e66ec7f9ac597792f1d8e7c40d5c37b863e
AyaAbdallah <151135323+Aya0Ahmed@users.noreply.github.com>
2025-11-20 13:41:31 +0200

    Add service worker registration for Firebase messaging
    Register service worker for Firebase messaging and log registration status.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
+// تحديد ملف الـ service worker
+navigator.serviceWorker.register('/firebase-messaging-sw.js')
+  .then((registration) => {
+    console.log('Service Worker registered!', registration);
+  })
+  .catch((err) => {
+    console.error('Service Worker registration failed:', err);
+  });
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdZ5qUVdYOy3wlZ6jCOCPHU_j_io_PnS4",
